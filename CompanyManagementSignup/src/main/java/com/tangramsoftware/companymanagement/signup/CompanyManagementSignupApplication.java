edb723a47e8a5cc71e7b4634f813ba46dcbc637b
Dan Michaelis <tangramconsult@gmail.com>
2019-06-06 11:22:39 -0400

    Initial Spring Boot Independent Project for Signups

---
+++ CompanyManagementSignup/src/main/java/com/tangramsoftware/companymanagement/signup/CompanyManagementSignupApplication.java
@@ -0,0 +1,13 @@
+package com.tangramsoftware.companymanagement.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManagementSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManagementSignupApplication.class, args);
+	}
+
+}
