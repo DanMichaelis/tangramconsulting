f16e58d6aaa6854f2e8be1d2ab1309f3dae435ee
jiniya.ghosh <jiniya.ghosh@CPS-JGHOSH2.mshome.net>
2020-01-13 22:37:37 -0500

    updated for user harmonizer

--- workflowmetrics/src/main/java/com/dtcc/workflowmetrics/service/UserHarmonizerService.java
+++ workflowmetrics/src/main/java/com/dtcc/workflowmetrics/service/UserHarmonizerService.java
@@ -1,19 +1,104 @@
 package com.dtcc.workflowmetrics.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dtcc.workflowmetrics.entity.EventUser;
 import com.dtcc.workflowmetrics.entity.EventUserCustomField;
+import com.dtcc.workflowmetrics.entity.MetricsItems;
+import com.dtcc.workflowmetrics.entity.MetricsItemsCustomField;
+import com.dtcc.workflowmetrics.entity.MetricsItemsStatusTransition;
+import com.dtcc.workflowmetrics.entity.MetricsItemsTStatusTransition;
+import com.dtcc.workflowmetrics.metricsitems.jira.common.CustomField;
 import com.dtcc.workflowmetrics.metricsitems.jira.common.User;
+import com.dtcc.workflowmetrics.metricsitems.jira.webhook.WebhookData;
+import com.dtcc.workflowmetrics.metricsitems.jira.webhook.WebhookIssue;
+import com.dtcc.workflowmetrics.metricsitems.jira.webhook.WebhookTransition;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class UserHarmonizerService {
-
+	
 	@Transactional
-	public final EventUser harmonizeJiraUser(User user) {
+	public final MetricsItems harmonizeJira(WebhookData data) {
+		
+		EventUser eventUser = harmonizeJiraUser(data.getUser());
+		
+		MetricsItems metricItem = harmonizeJiraIssue(data.getTransition(), data.getIssue());
+		return metricItem;
+		
+	}
+
+	
+	protected final MetricsItems harmonizeJiraIssue(WebhookTransition transition, WebhookIssue issue) {
+
+		int source = 12;
+		MetricsItems item = new MetricsItems();
+		
+		item.setItemId(transition.getTransitionId().toString());
+		item.setSourceSystemId(source);
+		item.setProjectId(issue.getFields().getProject().getId().toString());
+		item.setItemKey(issue.getFields().getCreator().getKey());
+		item.setItemSummary(issue.getFields().getSummary());
+		item.setItemDescription(issue.getFields().getDescription());
+		item.setItemCreateDate(Long.getLong(issue.getFields().getCreated()));
+		item.setItemCreator(issue.getFields().getCreator().getName());
+		item.setLastUpdateDate(null);
+		item.setLastUpdateUser(issue.getFields().getLastViewed());
+		
+		MetricsItemsTStatusTransition tStatusTransition = new MetricsItemsTStatusTransition();
+		
+		tStatusTransition.setItemId(transition.getTransitionId().toString());
+		tStatusTransition.setProjectId(issue.getFields().getProject().getId().toString());
+		tStatusTransition.setSourceSystemId(source);
+		tStatusTransition.setStatus(Integer.getInteger(issue.getFields().getStatus().getId()));
+		tStatusTransition.setTransitionDate(Long.getLong(issue.getFields().getCreated()));
+		
+		item.addTStatusTransition(tStatusTransition);
+		
+		MetricsItemsStatusTransition statusTransition = new MetricsItemsStatusTransition();
+		
+		statusTransition.setItemId(transition.getTransitionId().toString());
+		statusTransition.setProjectId(issue.getFields().getProject().getId().toString());
+		statusTransition.setSourceSystemId(source);
+		statusTransition.setFromStatus(transition.getFrom_status());
+		statusTransition.setToStatus(transition.getTo_status());
+		statusTransition.setTransitionDate(Long.getLong(issue.getFields().getCreated()));
+		
+		item.addStatusTransition(statusTransition);
+		
+		Map<String,CustomField> custField = issue.getFields().getCustomFields();
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		for (Map.Entry<String, CustomField> cust : custField.entrySet()) {
+
+			MetricsItemsCustomField metricCustomField = new MetricsItemsCustomField();
+			
+			metricCustomField.setItemId(transition.getTransitionId().toString());
+			metricCustomField.setSourceSystemId(source);
+			metricCustomField.setFieldName(cust.getKey());
+			metricCustomField.setFieldDatatype("CustomField");
+			try {
+				metricCustomField.setFieldValue(objectMapper.writeValueAsString(cust.getValue()));
+			} catch (JsonProcessingException e) {
+				System.out.println("JSON writing error");
+			}
+			metricCustomField.setCreateDate(Long.getLong(issue.getFields().getCreated()));
+			
+			item.addCustomField(metricCustomField);
+		}		
+		
+		
+		
+		return item;
+	}
+	
+	
+	protected final EventUser harmonizeJiraUser(User user) {
 		
 		int source = 12;
 		EventUser evntUsr = new EventUser();
