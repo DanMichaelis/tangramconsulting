ef953293a455e5042eb50e30e444eb884ee62acf
Dan Michaelis <tangramconsult@gmail.com>
2020-01-15 13:43:28 -0500

    Adding sample Mockito call

--- workflowmetrics/src/test/java/com/dtcc/workflowmetrics/service/TestSourceSystemDaoService.java
+++ workflowmetrics/src/test/java/com/dtcc/workflowmetrics/service/TestSourceSystemDaoService.java
@@ -1,8 +1,10 @@
 package com.dtcc.workflowmetrics.service;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.junit.After;
@@ -11,28 +13,36 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.AdditionalMatchers;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.dtcc.workflowmetrics.dao.SourceSystemDao;
 import com.dtcc.workflowmetrics.entity.SourceSystem;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestSourceSystemDaoService {
 
-    @Autowired
-    SourceSystemDaoService daoSvc;
-    
+    @Mock
+    private SourceSystemDao dao;
+
+    @InjectMocks
+    SourceSystemDaoService daoSvc = new SourceSystemDaoService();
+
     private static final SourceSystem JIRA = new SourceSystem(1, "Jira");
     private static final SourceSystem SNOW = new SourceSystem(2, "SNOW");
     private static final SourceSystem SERENA = new SourceSystem(3, "SERENA");
-    
+
     private static final ArrayList<SourceSystem> sourceSystems = new ArrayList<SourceSystem>();
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        
+
     }
 
     @AfterClass
@@ -46,6 +56,11 @@ public class TestSourceSystemDaoService {
         sourceSystems.add(JIRA);
         sourceSystems.add(SNOW);
         sourceSystems.add(SERENA);
+
+        when(dao.findById(JIRA.getId())).thenReturn(Optional.of(JIRA));
+        when(dao.findById(SNOW.getId())).thenReturn(Optional.of(SNOW));
+        when(dao.findById(SERENA.getId())).thenReturn(Optional.of(SERENA));
+
     }
 
     @After
@@ -55,14 +70,14 @@ public class TestSourceSystemDaoService {
     @Test
     public void testSave() {
         SourceSystem newJira = daoSvc.save(JIRA);
-        
+
         assertEquals(JIRA.getId(), newJira.getId());
         assertEquals(JIRA.getDescription(), newJira.getDescription());
     }
 
     @Test
     public void testSaveAll() {
-        
+
         Iterable<SourceSystem> savedSourceSystems = daoSvc.saveAll(sourceSystems);
         compareListWithReturnList(sourceSystems, savedSourceSystems);
     }
@@ -73,6 +88,20 @@ public class TestSourceSystemDaoService {
         Optional<SourceSystem> optS = daoSvc.findById(JIRA.getId().intValue());
         SourceSystem s = optS.get();
         assertEquals(JIRA, s);
+
+        optS = daoSvc.findById(SERENA.getId().intValue());
+        s = optS.get();
+        assertEquals(SERENA, s);
+
+        Boolean failed = true;
+        try {
+            optS = daoSvc.findById(1000);
+            s = optS.get();
+        } catch (NoSuchElementException e) {
+         failed = false;   
+        }
+        assertFalse(failed);
+        
     }
 
     @Test
@@ -88,19 +117,19 @@ public class TestSourceSystemDaoService {
         Iterable<SourceSystem> savedSourceSystems = daoSvc.findAll();
 
         compareListWithReturnList(sourceSystems, savedSourceSystems);
-        
+
     }
 
     @Test
     public void testFindAllById() {
         daoSvc.saveAll(sourceSystems);
-        
+
         ArrayList<Integer> ids = new ArrayList<Integer>();
-        
+
         for (SourceSystem s : sourceSystems) {
             ids.add(Integer.valueOf(s.getId()));
         }
-        
+
         Iterable<SourceSystem> savedSourceSystems = daoSvc.findAllById(ids);
         compareListWithReturnList(sourceSystems, savedSourceSystems);
     }
@@ -135,7 +164,7 @@ public class TestSourceSystemDaoService {
         Iterable<SourceSystem> rss = daoSvc.findAll();
         ArrayList<SourceSystem> rssa = new ArrayList<SourceSystem>();
         rss.forEach(rssa::add);
-        
+
         for (SourceSystem s : sourceSystems) {
             if (s.equals(JIRA)) {
                 assertFalse(rssa.contains(s));
@@ -149,15 +178,15 @@ public class TestSourceSystemDaoService {
     public void testDeleteAllIterableOfQextendsT() {
         daoSvc.saveAll(sourceSystems);
         ArrayList<SourceSystem> deletableSystems = new ArrayList<SourceSystem>();
-        
+
         deletableSystems.add(JIRA);
         deletableSystems.add(SNOW);
-        
+
         daoSvc.deleteAll(deletableSystems);
         Iterable<SourceSystem> rss = daoSvc.findAll();
         ArrayList<SourceSystem> rssa = new ArrayList<SourceSystem>();
         rss.forEach(rssa::add);
-        
+
         for (SourceSystem s : sourceSystems) {
             if (s.equals(JIRA) || s.equals(SNOW)) {
                 assertFalse(rssa.contains(s));
@@ -173,19 +202,19 @@ public class TestSourceSystemDaoService {
         daoSvc.deleteAll();
         assertEquals(0, daoSvc.count());
     }
-    
+
     private void compareListWithReturnList(Iterable<SourceSystem> originalList, Iterable<SourceSystem> returnedList) {
         ArrayList<SourceSystem> originalArray = new ArrayList<SourceSystem>();
         originalList.forEach(originalArray::add);
         ArrayList<SourceSystem> returnedArray = new ArrayList<SourceSystem>();
         returnedList.forEach(returnedArray::add);
-        
+
         ArrayList<SourceSystem> inOriginalButNotInReturned = new ArrayList<SourceSystem>(originalArray);
         ArrayList<SourceSystem> inReturnedButNotInOriginal = new ArrayList<SourceSystem>(returnedArray);
-        
+
         inOriginalButNotInReturned.removeAll(returnedArray);
         inReturnedButNotInOriginal.removeAll(originalArray);
-        
+
         assertEquals(0, inOriginalButNotInReturned.size());
         assertEquals(0, inReturnedButNotInOriginal.size());
     }
